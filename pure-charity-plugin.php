a542d7b821570188c7818c8dd06d059bdd05314a
Oleksandr Zemlianoi <raaccoon@gmail.com>
2017-01-21 12:36:17 +0200

    fix

--- pure-charity-plugin.php
+++ pure-charity-plugin.php
@@ -18,7 +18,6 @@
  * Description:       The plugin for Pure Charity API integration
  * Version:           1.0.0
  * Author:            <a href="http://purecharity.com">Pure Charity</a>
- * Author2:           <a href="http://zemlianoi.com">Aleskandr Zemlianoi</a>
  * Author URI:        http://purecharity.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
@@ -35,26 +34,4 @@ define( 'PURECHARITY_BASE_PATH', plugin_dir_path( __FILE__ ) );
 
 require_once PURECHARITY_BASE_PATH . 'includes/purecharity.class.php';
 
-//add_filter('extra_plugin_headers', 'add_extra_headers');
-//
-//function add_extra_headers(){
-//    return array('Author2');
-//}
-//
-//add_filter('plugin_row_meta', 'filter_authors_row_meta', 1, 4);
-//
-//function filter_authors_row_meta($plugin_meta, $plugin_file, $plugin_data, $status ){
-//
-//    if(empty($plugin_data['Author'])){
-//        return $plugin_meta;
-//    }
-//
-//
-//    if ( !empty( $plugin_data['Author2'] ) ) {
-//        $plugin_meta[1] = $plugin_meta[1] . ', ' . $plugin_data['Author2'];
-//    }
-//
-//
-//    return $plugin_meta;
-//}
 ?>
